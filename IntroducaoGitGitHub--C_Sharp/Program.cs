9e284c917af6e34199f7a72ac51e19ba7e4babb7
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-06-11 17:42:13 -0300

    Initial commit

---
+++ IntroducaoGitGitHub--C_Sharp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace IntroducaoGitGitHub__C_Sharp {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine("Yes, that's it, the World said hello, you're a luck guy and your future is gonna be awesome");
+        }
+    }
+}
