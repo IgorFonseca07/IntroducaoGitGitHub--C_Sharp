80cc7671914f65d14ed538d24ade73673b66ebd1
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-06-11 18:00:11 -0300

    Started comments

--- IntroducaoGitGitHub--C_Sharp/Program.cs
+++ IntroducaoGitGitHub--C_Sharp/Program.cs
@@ -7,6 +7,23 @@ namespace IntroducaoGitGitHub__C_Sharp {
             Console.WriteLine();
             Console.WriteLine("Yes, that's it, the World said hello, you're a luck guy and your future is gonna be awesome");
             Console.WriteLine("Have a great day!");
+
+
+            // PowerShell/CMD:
+            // ren git.txt .gitignore (depois copiar lá pra dentro todo o texto de gitignore para Visual Studio em github/Gitignore
+
+            // GitBASH:
+            // git init
+            // git config --global user.name ""
+            // git config --global user.email ""
+            // git status
+            // git add .
+            // git commit -m "" (caso esqueça de colocar um comentário, para sair do editor VIM, tecle ESC e depois-> :q!)
+            // git log
+            // git log --oneline
+
+
+
         }
     }
 }
